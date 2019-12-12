36a4e14bd927b5192ed8c870a9f6d34510d42f88
Kevin Trieu <dragn194@gmail.com>
2019-12-11 19:00:09 -0500

    Add a featured articles view to issues

--- content/views.py
+++ content/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.http import Http404
+from django.db.models.functions import Length
 
 
 from common.views import BaseView
@@ -34,6 +35,31 @@ class IssueView(BaseView):
 
     template_name = 'content/issue.html'
 
+    NUM_FEATURED = 3
+
+    def get_featured_articles(self, issue):
+        """Get the featured articles for this issue. This works on a priority system:
+        First, the article of the issue, if we have one.
+        Then, any promo articles, if we have any.
+        Finally, random articles we have that aren't already chosen.
+        We do this until we have NUM_FEATURED articles.
+        """
+        featured = list()
+        article_of_issue = issue.article_set.filter(is_article_of_issue=True)
+        if article_of_issue.exists():
+            featured.append(article_of_issue.first())
+        promo_articles = issue.article_set.filter(is_promo=True)
+        if promo_articles.exists():
+            featured.extend(promo_articles)
+        if len(featured) >= self.NUM_FEATURED:
+            return featured[:self.NUM_FEATURED]
+        else:
+            articles_needed = self.NUM_FEATURED - len(featured)
+            feature_ids = [article.id for article in featured]
+            articles = issue.article_set.exclude(id__in=feature_ids)
+            featured.extend(articles[:articles_needed])
+            return featured
+
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         volume = kwargs['volume']
@@ -42,4 +68,5 @@ class IssueView(BaseView):
             ctx['issue'] = Issue.objects.get(volume_num=volume, issue_num=issue)
         except Issue.DoesNotExist:
             raise Http404
+        ctx['featured_articles'] = self.get_featured_articles(ctx['issue'])
         return ctx
