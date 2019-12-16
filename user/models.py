058d4d270683e423d1f45ea605c8cd3686ce75ac
Kevin Trieu <dragn194@gmail.com>
2019-12-16 02:27:59 -0500

    Add an author name field to our custom user

--- user/models.py
+++ user/models.py
@@ -3,4 +3,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class SluglineUser(AbstractUser):
-    pass
+
+    """Articles written by this user will use this name by default."""
+    writer_name = models.CharField(max_length=255)    
+    
