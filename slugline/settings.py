9518a8f0e3198d32b76f8cc39bdfb0eb07f11004
Kevin Trieu <dragn194@gmail.com>
2020-01-05 13:32:09 -0500

    Install Django Sass Processor

--- slugline/settings.py
+++ slugline/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sass_processor',
     'common',
     'content',
     'user'
@@ -126,7 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'sass_processor.finders.CssFinder',
+]
 
 # Media, uploaded article images, issue PDFs, etc.
 
