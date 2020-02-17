7578b124ff066f1c741889bc9f61027086219726
Kevin Trieu <dragn194@gmail.com>
2020-02-17 10:56:51 -0500

    Add the auth view to the URL conf

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from user.views import login_view, logout_view
+from user.views import login_view, logout_view, auth_view
 
 urlpatterns = [
     path('login/', login_view),
-    path('logout/', logout_view)
+    path('logout/', logout_view),
+    path('auth/', auth_view)
 ]
