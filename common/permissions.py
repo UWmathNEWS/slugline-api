b10f356d1e350c5b1495a0e1c4b8a1c1ba5132e7
Terry Chen <tyxchen@outlook.com>
2020-03-17 12:00:21 -0400

    API calls should not fail server-side when made anonymously

--- common/permissions.py
+++ common/permissions.py
@@ -1,7 +1,9 @@
 from rest_framework.permissions import BasePermission
 
+from user.models import SluglineUser
+
 
 class IsEditor(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and (request.user.is_editor or request.user.is_staff))
+        return bool(isinstance(request.user, SluglineUser) and (request.user.is_editor or request.user.is_staff))
 
