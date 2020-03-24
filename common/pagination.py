c411003d4f8e1b3de46dbea2e1ffabcad9bd0f66
Kevin Trieu <ktrieu@secretmission.io>
2020-03-23 21:53:41 -0400

    Return root-relative URLS from the pagination
    If we return absolute URLs, it messes with create-react-app's proxying,
    so we strip the domain and such before we return them.

--- common/pagination.py
+++ common/pagination.py
@@ -1,5 +1,21 @@
+from urllib.parse import urlparse, urlunparse
+
 from rest_framework.pagination import LimitOffsetPagination
 from common.response import Response
+
+
+def to_relative_url(url):
+    parse_result = urlparse(url)
+    return urlunparse(
+        (
+            "",
+            "",
+            parse_result.path,
+            parse_result.params,
+            parse_result.query,
+            parse_result.fragment,
+        )
+    )
 
 
 class SluglinePagination(LimitOffsetPagination):
@@ -7,3 +23,12 @@ class SluglinePagination(LimitOffsetPagination):
         return Response(
             super(SluglinePagination, self).get_paginated_response(data).data
         )
+
+    def get_next_link(self):
+        link = super().get_next_link()
+        # remove the domain
+        return to_relative_url(link)
+
+    def get_previous_link(self):
+        link = super().get_previous_link()
+        return to_relative_url(link)
