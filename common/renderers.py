9459ebc4322964ade8e71a80a9f3d92170b50adb
Kevin Trieu <ktrieu@secretmission.io>
2020-03-29 00:02:20 -0400

    Remove print statement

--- common/renderers.py
+++ common/renderers.py
@@ -3,7 +3,6 @@ from rest_framework.renderers import JSONRenderer
 
 class SluglineRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
-        print(renderer_context["response"].exception)
         rendered_json = {
             "success": renderer_context["response"].exception == False,
             "data": data,
