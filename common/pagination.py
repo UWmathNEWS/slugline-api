592bea9a97a736e2f4b456052f17bda5d964a851
Kevin Trieu <ktrieu@secretmission.io>
2020-03-29 19:18:17 -0400

    Add custom paginator response

--- common/pagination.py
+++ common/pagination.py
@@ -1,5 +1,16 @@
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
-class SluglinePagination(LimitOffsetPagination):
-    pass
+class SluglinePagination(PageNumberPagination):
+    def get_paginated_response(self, data):
+        return Response(
+            data={
+                "count": self.page.paginator.count,
+                "page": self.page.number,
+                "num_pages": self.page.paginator.num_pages,
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "results": data,
+            }
+        )
