f18483d8ed86cf92b030d4a9719c6bb6b2ca33f4
Kevin Trieu <ktrieu@secretmission.io>
2020-03-29 23:12:21 -0400

    Make the pagination return relative URLs
    This makes it work with CRA's proxy on the front end

--- common/pagination.py
+++ common/pagination.py
@@ -1,8 +1,23 @@
+from urllib.parse import urlparse, urlunparse
+
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
 
+def absolute_url_to_relative(url):
+    result = urlparse(url)
+    return urlunparse(
+        ["", "", result.path, result.params, result.query, result.fragment]
+    )
+
+
 class SluglinePagination(PageNumberPagination):
+    def get_next_link(self):
+        return absolute_url_to_relative(super().get_next_link())
+
+    def get_previous_link(self):
+        return absolute_url_to_relative(super().get_previous_link())
+
     def get_paginated_response(self, data):
         return Response(
             data={
