ead15c45e88c799217a37b84608ca31f0f13f687
Kevin Trieu <kevin.trieu5813@gmail.com>
2020-05-05 16:42:04 -0400

    Add the status code to all responses

--- common/renderers.py
+++ common/renderers.py
@@ -3,16 +3,22 @@ from rest_framework.renderers import JSONRenderer
 
 class SluglineRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
-        if renderer_context["response"].exception:
+        response = renderer_context["response"]
+        if response.exception:
             # convert singular detail arguments into arrays
             if "detail" in data and isinstance(data["detail"], str):
                 data["detail"] = [data["detail"]]
             rendered_json = {
                 "success": False,
+                "status_code": response.status_code,
                 "error": data,
             }
         else:
-            rendered_json = {"success": True, "data": data}
+            rendered_json = {
+                "success": True,
+                "status_code": response.status_code,
+                "data": data,
+            }
         return super().render(
             rendered_json,
             accepted_media_type=accepted_media_type,
