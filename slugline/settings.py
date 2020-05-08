05191ec59ce34ea748321db6ef073efe2a00e9a2
Kevin Trieu <kevin.trieu5813@gmail.com>
2020-05-08 17:18:05 -0400

    Remove some dependencies we weren't using

--- slugline/settings.py
+++ slugline/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "sass_processor",
     "rest_framework",
     "rest_framework.authtoken",
     "common",
@@ -136,7 +135,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-    "sass_processor.finders.CssFinder",
 ]
 
 # Media, uploaded article images, issue PDFs, etc.
