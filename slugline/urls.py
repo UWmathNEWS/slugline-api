795cc736e5a8bc2f45f73c672bf8d4b58392a75f
Vincent Macri <developer@vincemacri.ca>
2020-07-06 23:14:37 -0400

    Formatting

--- slugline/urls.py
+++ slugline/urls.py
@@ -15,17 +15,46 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.urls import path, include
 from django.shortcuts import render
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 import content.urls
 import content.views
 import user.urls
 import user.views
 
+swagger_info = openapi.Info(
+    title="goosePRESS API",
+    default_version="v1",
+    description="API for goosePRESS",
+    # terms_of_service="https://www.google.com/policies/terms/",
+    # contact=openapi.Contact(email="contact@snippets.local"),
+    # license=openapi.License(name="AGPL v3+"),
+)
+
+schema_view = get_schema_view(public=True, permission_classes=(permissions.AllowAny,),)
+
 urlpatterns = [
+    url(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    url(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    url(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
     path("api/", include(content.urls)),
     path("api/", include(user.urls)),
     path("admin/", admin.site.urls),
