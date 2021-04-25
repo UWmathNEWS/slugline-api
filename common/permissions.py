25afba340f67b74ae948bd1691f6acf5ebe70ea6
Kevin Trieu <kevin.trieu5813@gmail.com>
2021-04-25 16:26:37 -0400

    Add a new permission class that handles both read and write permissions

--- common/permissions.py
+++ common/permissions.py
@@ -1,7 +1,25 @@
 from user.groups import COPYEDITOR_GROUP, EDITOR_GROUP
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 from user.models import SluglineUser
+
+
+class SluglinePermission(BasePermission):
+    def __init__(self, read_perm, write_perm):
+        self.read_perm = read_perm
+        self.write_perm = write_perm
+
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return self.read_perm.has_permission(request, view)
+        else:
+            return self.write_perm.has_permission(request, view)
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return self.read_perm.has_permission(request, view, obj)
+        else:
+            return self.write_perm.has_permission(request, view, obj)
 
 
 class IsContributorOrAbove(BasePermission):
