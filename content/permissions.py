ef999dfb6485f9b3820922db4d9c8cba82d5ebe3
Kevin Trieu <kevin.trieu5813@gmail.com>
2021-04-26 21:03:01 -0400

    Remove imports we're not using

--- content/permissions.py
+++ content/permissions.py
@@ -1,8 +1,5 @@
 from common.permissions import SluglinePermission
 
-from user.models import SluglineUser
-from user.groups import EDITOR_GROUP, CONTRIBUTOR_GROUP, COPYEDITOR_GROUP
-
 
 class IsArticlePublished(SluglinePermission):
     def has_object_permission(self, request, view, article):
